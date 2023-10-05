5a6b7677df168fb15dd1c3f1d73b3389b9533997
Crisiroid <csiroid@gmail.com>
2023-10-05 16:56:45 +0330

    Update: Added slides to Main Page

--- lib/View/BottomNavigationBarPages/HomePage.dart
+++ lib/View/BottomNavigationBarPages/HomePage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_final_fields, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:shipify/View/Decoration/Slides.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,8 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home page"),
-    );
+    return SingleChildScrollView(
+        child: Center(
+      child: Column(children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [
+            Slides(
+                slideText: "hello world", imageUrl: "imageUrl", onTap: () {}),
+            Slides(
+                slideText: "hello world", imageUrl: "imageUrl", onTap: () {}),
+            Slides(
+                slideText: "hello world", imageUrl: "imageUrl", onTap: () {}),
+          ]),
+        )
+      ]),
+    ));
   }
 }
---
+++ lib/View/Decoration/Slides.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Slides extends StatelessWidget {
+  final String slideText;
+  final String imageUrl;
+  final VoidCallback onTap;
+  const Slides(
+      {super.key,
+      required this.slideText,
+      required this.imageUrl,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      width: MediaQuery.of(context).size.width - 40,
+      height: 250,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 250,
+            height: 250,
+            color: Colors.white,
+          ),
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.black.withAlpha(0),
+                  Colors.black12,
+                  Colors.black45
+                ],
+              ),
+            ),
+            child: Text(
+              slideText,
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
